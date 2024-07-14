eccd8830daaf9f3623f8bec5a26a2f7614fe716f
batsalee <ufo2748@gmail.com>
2024-07-14 13:35:30 +0900

    리팩토링을 위한 초기 세팅 및 CMake 구조로 변경

--- Qt/TaskManager/FolderOpener.cpp
+++
@@ -1,7 +0,0 @@
-#include "FolderOpener.h"
-
-void FolderOpener::openFolder(const QString& path)
-{
-    QDesktopServices::openUrl(QUrl::fromLocalFile(QDir::toNativeSeparators(QCoreApplication::applicationDirPath() + path)));
-    // 설치경로에 한글이 있을것도 감안해서 toNativeSeparators로 인코딩
-}
